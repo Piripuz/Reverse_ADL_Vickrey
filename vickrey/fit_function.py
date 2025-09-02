e9f91f22a95faa1733222376c56ce18c4f111251
Pietro Giardina <s233663@dtu.dk>
2025-09-02 23:08:33 +0200

    Implemented skewed gaussian fitting

--- vickrey/fit_function.py
+++ vickrey/fit_function.py
@@ -1,11 +1,12 @@
+import os
+
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import os
+from scipy.optimize import curve_fit
+from scipy.stats import skewnorm, gennorm
 
 from jax import grad, vmap
 from jax import numpy as jnp
-from scipy.optimize import curve_fit
 
 import jax
 jax.config.update('jax_enable_x64', True)
@@ -56,43 +57,83 @@ def func(a, b, c, p, off):
                       ]) + off
     return inner_func
 
-def fit_to_data(x, y, init=None):
-    to_fit = lambda x, a1, a2, a3, b1, c1, c2, c3, p1, p2, off: \
-        func([a1, a2, a3], [b1], [c1, c2, c3], [p1, p2], off)(x)
-    if init == None:
-        crit_left = x[(y > 1.5*y.min()).argmax()]
-        crit_right = x[len(y) - (y > 1.5*y.min())[::-1].argmax()]
-        b = y.max()
+def fit_to_data(x, y, kind="hyperbola", init=None):
+    
+    supported=["hyperbola", "skewed_gaussian", "generalized_gaussian"]
+    if kind not in supported:
+        raise ValueError(f"Kind \"{kind}\" is not supported, as it is not among {supported}")
 
-        p_init = list(np.linspace(crit_left, crit_right, 4)[1:3])
-        off_init = y[0]
-        a_init = [.01, (p_init[1] - p_init[0])/(b - off_init), crit_left]
-        b_init = [b]
-        c_init = [*a_init[:2], crit_right]
-    else:
-        a_init = init[:3]
-        b_init = init[3] 
-        c_init = init[4:7]
-        p_init = init[7:9]
-        off_init = init[9]
+    if kind == "hyperbola":
+        to_fit = lambda x, a1, a2, a3, b1, c1, c2, c3, p1, p2, off: \
+            func([a1, a2, a3], [b1], [c1, c2, c3], [p1, p2], off)(x)
+        if init == None:
+            crit_left = x[(y > 1.5*y.min()).argmax()]
+            crit_right = x[len(y) - (y > 1.5*y.min())[::-1].argmax()]
+            b = y.max()
 
-    popt, _ = curve_fit(to_fit,
-                     x,
-                     y,
-                     a_init + b_init + c_init + p_init + [off_init],
-                     bounds=(
-                         [0, 0, -np.inf] + [-np.inf]*len(b_init) + [0, 0, -np.inf, 0, 0, 0],
-                         [np.inf]*(9+len(b_init))
-                     ))
-    a, b, c, p, off = popt[:3], popt[3:(3 + len(b_init))], popt[-6:-3], popt[-3:-1], popt[-1]
-    print(
-        f"\
-        Converged to:\n\
-        a = {a}\n\
-        b = {b}\n\
-        c = {c}\n\
-        p = {p}\n\
-        offset = {off}\
-        "
-    )
-    return func(a, b, c, p, off)
+            p_init = list(np.linspace(crit_left, crit_right, 4)[1:3])
+            off_init = y[0]
+            a_init = [.01, (p_init[1] - p_init[0])/(b - off_init), crit_left]
+            b_init = [b]
+            c_init = [*a_init[:2], crit_right]
+        else:
+            a_init = init[:3]
+            b_init = init[3] 
+            c_init = init[4:7]
+            p_init = init[7:9]
+            off_init = init[9]
+
+        popt, _ = curve_fit(to_fit,
+                            x,
+                            y,
+                            a_init + b_init + c_init + p_init + [off_init],
+                            bounds=(
+                                [0, 0, -np.inf] + [-np.inf]*len(b_init) + [0, 0, -np.inf, 0, 0, 0],
+                                [np.inf]*(9+len(b_init))
+                            ))
+        a, b, c, p, off = popt[:3], popt[3:(3 + len(b_init))], popt[-6:-3], popt[-3:-1], popt[-1]
+        print(
+            f"\
+            Converged to:\n\
+            a = {a}\n\
+            b = {b}\n\
+            c = {c}\n\
+            p = {p}\n\
+            offset = {off}\
+            "
+        )
+        return func(a, b, c, p, off)
+    elif kind == "skewed_gaussian":
+        if init == None:
+            crit_left = x[(y > 1.5*y.min()).argmax()]
+            crit_right = x[len(y) - (y > 1.5*y.min())[::-1].argmax()]
+
+            a_init = 0
+            mu_init = x[y.argmax()]
+            sigma_init = (crit_right - crit_left)/4
+            off_init = y[0]
+            scale_init =  (y.max() - off_init) / skewnorm.pdf(mu_init, a_init, mu_init, sigma_init)
+        else:
+            a_init, mu_init, sigma_init, scale_init, off_init = init
+
+        to_fit = lambda x, a, mu, sigma, scale, off: skewnorm.pdf(x, a, mu, sigma)*scale + off
+        popt, _ = curve_fit(
+            to_fit,
+            x,
+            y,
+            [a_init, mu_init, sigma_init, scale_init, off_init],
+        )
+        a, mu, sigma, scale, off = popt
+        print(
+            f"\
+            Converged to:\n\
+            a = {a}\n\
+            mu = {mu}\n\
+            sigma = {sigma}\n\
+            scale = {scale}\n\
+            offset = {off}\
+            "
+        )
+        return lambda x: to_fit(x, a, mu, sigma, scale, off)
+    elif kind == "generalized_gaussian":
+        raise NotImplementedError()

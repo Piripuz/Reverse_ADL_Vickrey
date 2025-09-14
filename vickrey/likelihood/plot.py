bc6dbae8be76f389717dd8c86b8f4570290157a5
Pietro Giardina <s233663@dtu.dk>
2025-09-14 16:18:03 +0200

    Improved plotting for different contours

--- vickrey/likelihood/plot.py
+++ vickrey/likelihood/plot.py
@@ -1,43 +1,49 @@
 import matplotlib.pyplot as plt
 
+
 import jax.numpy as jnp
 from jax import vmap
 
 from vickrey.likelihood.likelihood import total_log_lik
 
+plt.rcParams["text.usetex"] = True
+
 
 def plot_contour(
     tt,
     t_as,
+    par,
     ax=None,
-    x_name="mu_beta",
-    y_name="mu_gamma",
-    x_bounds=[0.01, 0.99],
-    y_bounds=[0.01, 3.0],
-    par=[None, None, 9.5, 0.1, 1.5],
+    x_bounds=None,
+    y_bounds=None,
 ):
-    names = ["mu_beta", "mu_gamma", "mu_star", "sigma", "sigma_star"]
-    if x_name not in names:
+    indices = [i for i, v in enumerate(par) if not v]
+    if len(indices) != 2:
         raise ValueError(
-            f"The value {x_name} was not recognized as a parameter name"
+            f"The parameter list must have two None values.\n\
+            {par} has instead {len(indices)}"
         )
-    if y_name not in names:
-        raise ValueError(
-            f"The value {y_name} was not recognized as a parameter name"
-        )
+    x_index, y_index = indices
+    bounds = [(0.01, 0.6), (0.01, 0.6), (6.5, 11.0), (0.01, 0.6), (0.1, 2.0)]
+    if not x_bounds:
+        x_bounds = bounds[x_index]
+    if not y_bounds:
+        y_bounds = bounds[y_index]
 
     def log_lik(x, y):
-        x_index, y_index = names.index(x_name), names.index(y_name)
         par[x_index] = x
         par[y_index] = y
         return total_log_lik(tt, t_as)(*par)
 
-    x_contour = jnp.linspace(*x_bounds, 201)
-    y_contour = jnp.linspace(*y_bounds, 200)
+    x_contour = jnp.linspace(*x_bounds, 81)
+    y_contour = jnp.linspace(*y_bounds, 80)
     matrix_actual = vmap(vmap(log_lik, (0, None)), (None, 0))(
         x_contour, y_contour
     )
     if not ax:
         ax = plt.gca()
     ax.contour(x_contour, y_contour, matrix_actual, levels=50)
+    names = [r"\mu_\beta", r"\mu_\gamma", r"\mu_t", r"\sigma", r"\sigma_t"]
+    ax.set_xlabel("$" + names[x_index] + "$")
+    ax.set_ylabel("$" + names[y_index] + "$")
     return ax

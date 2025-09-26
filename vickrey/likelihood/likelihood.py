eb825f88be5e6f609f4cce4d4459e09d5420c970
Pietro Giardina <s233663@dtu.dk>
2025-09-26 19:18:53 +0200

    Splitted likelihood function into three different functions

--- vickrey/likelihood/likelihood.py
+++ vickrey/likelihood/likelihood.py
@@ -16,6 +16,108 @@ from vickrey.likelihood.find_points import (
 )
 
 
+def _pdf_sched(mu, sigma):
+    def inner_pdf(x):
+        return jtruncnorm.pdf(
+            x, (0.01 - mu) / sigma, 10000, loc=mu, scale=sigma
+        )
+
+    return inner_pdf
+
+
+def _cdf_sched(mu, sigma):
+    def inner_cdf(x):
+        return jtruncnorm.cdf(
+            x, (0.01 - mu) / sigma, 10000, loc=mu, scale=sigma
+        )
+
+    return inner_cdf
+
+
+def _ot_lik(travel_time, t_a, mu_b, mu_g, mu_t, sigma, sigma_t):
+    cdf_b = _cdf_sched(mu_b, sigma)
+    cdf_g = _cdf_sched(mu_g, sigma)
+    # For computing the probability that a point is a kink minimum, an
+    # integral is computed as in the latex.
+    b0 = find_b0(t_a, travel_time)
+    g0 = find_g0(t_a, travel_time)
+    likelihood_kink = (
+        jnorm.pdf(t_a, mu_t, sigma_t) * (1 - cdf_b(b0)) * (1 - cdf_g(g0))
+    )
+    return likelihood_kink
+
+
+def _early_lik(travel_time, t_a, mu_b, mu_g, mu_t, sigma, sigma_t):
+    pdf_g = _pdf_sched(mu_g, sigma)
+    pdf_b = _pdf_sched(mu_b, sigma)
+
+    # Here, t_a is transformed so that it always returns a plausible
+    # result for an early or late arrival. If the actual t_a is not
+    # plausible, the pdf of beta will return zero and not yield any
+    # problem. These transformations are anyway necessary because
+    # impossible values cannot be fed to find_be and find_gi
+    # (functions to which this check could be delegated)
+
+    t_a_early = jnp.where(
+        jnp.logical_and(travel_time.df(t_a) > 0, travel_time.d2f(t_a) > 0),
+        t_a,
+        0,
+    )
+
+    def inner_int_early_cdf(x):
+        return jnorm.cdf(
+            jnp.minimum(
+                find_be(t_a_early, travel_time),
+                find_ts(travel_time.df(t_a_early), x, travel_time),
+            ),
+            mu_t,
+            sigma_t,
+        ) - jnorm.cdf(t_a_early, mu_t, sigma_t)
+
+    def inner_int_early(x):
+        return inner_int_early_cdf(x) * pdf_g(x)
+
+    x_gamma = jnp.linspace(1e-2, 3, 500)
+    int_early = trapezoid(vmap(inner_int_early)(x_gamma), x_gamma, axis=0)
+    lik_early = (
+        int_early * pdf_b(travel_time.df(t_a)) * relu(travel_time.d2f(t_a))
+    )
+    return lik_early
+
+
+def _late_lik(travel_time, t_a, mu_b, mu_g, mu_t, sigma, sigma_t):
+    pdf_g = _pdf_sched(mu_g, sigma)
+    pdf_b = _pdf_sched(mu_b, sigma)
+
+    t_a_late = jnp.where(
+        jnp.logical_and(travel_time.df(t_a) < 0, travel_time.d2f(t_a) > 0),
+        t_a,
+        24,
+    )
+
+    def inner_int_late_cdf(x):
+        return jnorm.cdf(t_a_late, mu_t, sigma_t) - jnorm.cdf(
+            jnp.maximum(
+                find_gi(t_a_late, travel_time),
+                find_ts(x, -travel_time.df(t_a_late), travel_time),
+            ),
+            mu_t,
+            sigma_t,
+        )
+
+    def inner_int_late(x):
+        return inner_int_late_cdf(x) * pdf_b(x)
+
+    x_beta = jnp.linspace(1e-2, 3, 500)
+
+    int_late = trapezoid(vmap(inner_int_late)(x_beta), x_beta, axis=0)
+
+    lik_late = (
+        int_late * pdf_g(-travel_time.df(t_a)) * relu(travel_time.d2f(t_a))
+    )
+    return lik_late
+
+
 def likelihood(travel_time, t_a, mu_b, mu_g, mu_t, sigma, sigma_t):
     """Find the likelihood of a single data point.
 
@@ -42,92 +144,11 @@ def likelihood(travel_time, t_a, mu_b, mu_g, mu_t, sigma, sigma_t):
             optimal arrival being equal to t_a
 
     """
-
-    # The truncated normals pdf and cdf are defined here
-    def cdf_b(b):
-        return jtruncnorm.cdf(
-            b, (0.01 - mu_b) / sigma, 10000, loc=mu_b, scale=sigma
-        )
-
-    def cdf_g(g):
-        return jtruncnorm.cdf(
-            g, (0.01 - mu_g) / sigma, 10000, loc=mu_g, scale=sigma
-        )
-
-    def pdf_b(b):
-        return jtruncnorm.pdf(b, (0.01 - mu_b) / sigma, 10000, mu_b, sigma)
-
-    def pdf_g(g):
-        return jtruncnorm.pdf(g, (0.01 - mu_g) / sigma, 10000, mu_g, sigma)
-
-    # For computing the probability that a point is a kink minimum, an
-    # integral is computed as in the latex.
-    b0 = find_b0(t_a, travel_time)
-    g0 = find_g0(t_a, travel_time)
-    likelihood_kink = (
-        jnorm.pdf(t_a, mu_t, sigma_t) * (1 - cdf_b(b0)) * (1 - cdf_g(g0))
+    lik_early = _early_lik(travel_time, t_a, mu_b, mu_g, mu_t, sigma, sigma_t)
+    lik_late = _late_lik(travel_time, t_a, mu_b, mu_g, mu_t, sigma, sigma_t)
+    likelihood_kink = _ot_lik(
+        travel_time, t_a, mu_b, mu_g, mu_t, sigma, sigma_t
     )
-
-    # Now for internal minima: we just follow the equation in the latex.
-
-    # Here, t_a is transformed so that it always returns a plausible
-    # result for an early or late arrival. If the actual t_a is not
-    # plausible, the pdf of beta will return zero and not yield any
-    # problem. These transformations are anyway necessary because
-    # impossible values cannot be fed to find_be and find_gi
-    # (functions to which this check could be delegated)
-
-    t_a_early = jnp.where(
-        jnp.logical_and(travel_time.df(t_a) > 0, travel_time.d2f(t_a) > 0),
-        t_a,
-        0,
-    )
-    t_a_late = jnp.where(
-        jnp.logical_and(travel_time.df(t_a) < 0, travel_time.d2f(t_a) > 0),
-        t_a,
-        24,
-    )
-
-    def inner_int_early_cdf(x):
-        return jnorm.cdf(
-            jnp.minimum(
-                find_be(t_a_early, travel_time),
-                find_ts(travel_time.df(t_a_early), x, travel_time),
-            ),
-            mu_t,
-            sigma_t,
-        ) - jnorm.cdf(t_a_early, mu_t, sigma_t)
-
-    def inner_int_early(x):
-        return inner_int_early_cdf(x) * pdf_g(x)
-
-    x_gamma = jnp.linspace(1e-2, 3, 500)
-    int_early = trapezoid(vmap(inner_int_early)(x_gamma), x_gamma, axis=0)
-    lik_early = (
-        int_early * pdf_b(travel_time.df(t_a)) * relu(travel_time.d2f(t_a))
-    )
-
-    def inner_int_late_cdf(x):
-        return jnorm.cdf(t_a_late, mu_t, sigma_t) - jnorm.cdf(
-            jnp.maximum(
-                find_gi(t_a_late, travel_time),
-                find_ts(x, -travel_time.df(t_a_late), travel_time),
-            ),
-            mu_t,
-            sigma_t,
-        )
-
-    def inner_int_late(x):
-        return inner_int_late_cdf(x) * pdf_b(x)
-
-    x_beta = jnp.linspace(1e-2, 3, 500)
-
-    int_late = trapezoid(vmap(inner_int_late)(x_beta), x_beta, axis=0)
-
-    lik_late = (
-        int_late * pdf_g(-travel_time.df(t_a)) * relu(travel_time.d2f(t_a))
-    )
-
     likelihood_internal = lik_early + lik_late
     likelihood = likelihood_kink + likelihood_internal
     return jnp.maximum(likelihood, 1e-31)
